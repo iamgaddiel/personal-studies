7e9c216fae949676a5f30c6f37d7200a09acb8e9
iamgaddiel <ighotagaddiel98@gmail.com>
2022-08-24 17:32:33 -0400

    started learning ReactNative

---
+++ ReactNative/TestReminderApp/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.js to start working on your app no!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'teal',
+  },
+  text: {
+    color: '#fff'
+  }
+});
